442418757b576ed6150c354bfb5d3bb0a414eaad
znako <127133685+Bagemich@users.noreply.github.com>
2023-03-31 00:14:12 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int z = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[z];
+void mas(int z)
+{
+    for (int i = 0; i < z; i++)
+    {
+        Console.Write($"Введите {i+1} элемент массива ");
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int kol(int[] Array)
+{
+    int i=0;
+    int sum = 0;
+    while (i < Array.Length)
+    {
+        if(Array[i]>0)
+        sum = sum + 1;
+        i = i + 1;
+    }
+    return sum;
+}
+mas(z);
+Console.WriteLine($"\n Чисел больше нуля: {kol(Array)}");
